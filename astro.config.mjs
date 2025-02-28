28f39633babef388bd728f634c5b20ccfc81469d
IvarsLV <ivars.plisko@gmail.com>
2025-02-28 16:33:30 +0200

    chore: Добавлена конфигурация для GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://betonblock.lv',
+  site: 'https://betonblock.github.io',
+  base: '/Betonblock',
   integrations: [tailwind()],
 });
