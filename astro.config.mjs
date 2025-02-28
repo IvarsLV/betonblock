ec1bf1d120a03494b4a2a827a6b502469c641287
IvarsLV <ivars.plisko@gmail.com>
2025-02-28 17:20:17 +0200

    feat: Добавлено динамическое изменение заголовка страницы при навигации между секциями

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://betonblock.github.io',
-  base: '/Betonblock',
+  base: '/',
   integrations: [tailwind()],
 });
