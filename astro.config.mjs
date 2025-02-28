f16d1b9ab1a4a48fa5847cbdbb2548de0694db26
IvarsLV <ivars.plisko@gmail.com>
2025-02-28 18:23:17 +0200

    feat: обновлено мобильное меню, улучшен дизайн и анимации

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://betonblock.github.io',
-  base: '/',
+  base: '/Betonblock',
+  build: {
+    assets: 'assets'
+  },
   integrations: [tailwind()],
+  vite: {
+    build: {
+      assetsDir: 'assets'
+    }
+  }
 });
